4cebb482a43dc3fdadd09ffa9dd828fdaac39cde
FlanChan <104259619+FlanChanXwO@users.noreply.github.com>
2026-04-20 00:22:26 +0800

    feat: update logging mechanism with prefixed logger and improved stack trace handling (#30)

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -1,5 +1,7 @@
 """Logger wrapper that adds plugin prefix to all log messages."""
 
+from __future__ import annotations
+
 from astrbot.api import logger as _logger
 
 
@@ -8,27 +10,36 @@ class PrefixedLogger:
 
     PREFIX = "[astrbot_plugin_rsshub] "
 
+    # Skip wrapper frame so astrbot log shows the real caller file:line.
+    CALLER_STACKLEVEL = 2
+
     def _add_prefix(self, msg):
         """Add prefix to message if it's a string."""
         return self.PREFIX + str(msg)
 
+    def _with_stacklevel(self, kwargs: dict) -> dict:
+        copied_kwargs = dict(kwargs)
+        if "stacklevel" not in copied_kwargs:
+            copied_kwargs["stacklevel"] = self.CALLER_STACKLEVEL
+        return copied_kwargs
+
     def debug(self, msg, *args, **kwargs):
-        _logger.debug(self._add_prefix(msg), *args, **kwargs)
+        _logger.debug(self._add_prefix(msg), *args, **self._with_stacklevel(kwargs))
 
     def info(self, msg, *args, **kwargs):
-        _logger.info(self._add_prefix(msg), *args, **kwargs)
+        _logger.info(self._add_prefix(msg), *args, **self._with_stacklevel(kwargs))
 
     def warning(self, msg, *args, **kwargs):
-        _logger.warning(self._add_prefix(msg), *args, **kwargs)
+        _logger.warning(self._add_prefix(msg), *args, **self._with_stacklevel(kwargs))
 
     def error(self, msg, *args, **kwargs):
-        _logger.error(self._add_prefix(msg), *args, **kwargs)
+        _logger.error(self._add_prefix(msg), *args, **self._with_stacklevel(kwargs))
 
     def exception(self, msg, *args, **kwargs):
-        _logger.exception(self._add_prefix(msg), *args, **kwargs)
+        _logger.exception(self._add_prefix(msg), *args, **self._with_stacklevel(kwargs))
 
     def critical(self, msg, *args, **kwargs):
-        _logger.critical(self._add_prefix(msg), *args, **kwargs)
+        _logger.critical(self._add_prefix(msg), *args, **self._with_stacklevel(kwargs))
 
 
 # Singleton instance for import
